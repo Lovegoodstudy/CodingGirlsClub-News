1bcf38e0e429281915681ee6d9dc945896f5a0dd
lovegood <287310886@qq.com>
2017-08-10 11:07:41 +0800

    addArticle complete v0.1

--- server/app.js
+++ server/app.js
@@ -1,25 +1,26 @@
-'use strict';
 let express = require('express');
 let orm = require('orm');
 let app = express();
+
 let bodyParser = require('body-parser');
-let path = require('path');
-let jsonParser = bodyParser.json;
+
+var jsonParser = bodyParser.json()
 let urlencodedParser = bodyParser.urlencoded({extended: true});
-let appRoot = path.join(__dirname, '/');
-//cookie的设置
-let session = require("express-session");
-let cookie = require("cookie-parser");
-app.configure(function() {
-    app.use(cookie());
-    app.use(session({
-        name: "final",
-        secret: "1234567",
-        cookie: {maxAge: 10000},   //过期时间 毫秒为单位
-        resave: true,        //每次触发后保存时间
-        rolling: true       // 最后一次触发后计时
-    }));
-});
+
+let path = require('path');
+let appRoot = path.join(__dirname, '/');//cookie的设置
+// let session = require("express-session");
+// let cookie = require("cookie-parser");
+// app.configure(function() {
+//     app.use(cookie());
+//     app.use(session({
+//         name: "final",
+//         secret: "1234567",
+//         cookie: {maxAge: 10000},   //过期时间 毫秒为单位
+//         resave: true,        //每次触发后保存时间
+//         rolling: true       // 最后一次触发后计时
+//     }));
+// });
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -27,7 +28,7 @@ app.all('*', function (req, res, next) {
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
 });
-app.use(orm.express(`sqlite:///home/zl/sqlites/manage`, {
+app.use(orm.express(`sqlite://${appRoot}database.db`, {
     define: function (db, models, next) {
         models.Manage = db.define("manage", {
             id:Number,
@@ -36,11 +37,13 @@ app.use(orm.express(`sqlite:///home/zl/sqlites/manage`, {
             email:String
         });
         models.News=db.define("news",{
+            id : Number,
             title:String,
             content:String,
             date:String
         });
         models.Blogs=db.define("blogs",{
+            id : Number,
             title:String,
             content:String,
             date:String
@@ -63,18 +66,10 @@ app.get('/manage/blogs',urlencodedParser,function (req,res) {
     let blogs = require('./getAllBlogs');
     blogs.getAllBlogs(req,res);
 });
-app.post('/imageUplode', function (req, res) {
 
-});
-app.post('article', jsonParser, function (req, res) {
-    
-    if (req.query.type === 'new') {
-        req.models.News.create({ title : req.body.title, content : req.body.content, date : })
-    }
-    if (req.query.type === 'blog') {
+var articleRouter = require('./articleRouter');
+app.use('/article', articleRouter);
 
-    }
-})
 var server = app.listen(8081, function () {
     var host = server.address().address;
     var port = server.address().port;
