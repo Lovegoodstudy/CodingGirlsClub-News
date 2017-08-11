8a873ea90b6bf4b3bd564d9f5c02832fcd6c5b5e
MyunFeiYang <420222aa>
2017-08-11 14:27:57 +0800

    init

--- server/app.js
+++ server/app.js
@@ -1,61 +1,77 @@
-"use strict"
 let express = require('express');
 let orm = require('orm');
 let app = express();
-let bodyPaser = require('body-parser');
-let path = require('path');
-let urlencodedParser = bodyPaser.urlencoded({extended: true});
-let appRoot = path.join(__dirname, '/');
-const nodemailer = require('nodemailer');
-app.use(express.static('public'));
 
+let path = require('path');
+let appRoot = path.join(__dirname, '/');
+
+//cookie的设置
+let session = require("express-session");
+let cookie = require("cookie-parser");
 
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.header("Content-Type", "application/json;charset=utf-8");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
+// 设置 Cookie
+app.use(cookie());
+app.use(session({
+    secret: '12345',
+    name: 'database',            //这里的name值得是cookie的name，默认cookie的name是：connect.sid
+    cookie: {max: 180 * 60 * 1000},
+    resave: false,
+    saveUninitialized: true,
+}));
 
-app.use(orm.express(`sqlite:///${appRoot}/codeGirlsClub.db`, {
+app.use(orm.express(`sqlite:///${appRoot}database.db`, {
     define: function (db, models, next) {
-        models.Manager = db.define("manager", {
+        models.Manager = db.define("manage", {
             id: Number,
-            nickname : String,
-            password : String,
-            email : String
+            manageName: String,
+            password: String,
+            email: String
         });
-        models.News = db.define('news', {
-            id : Number,
-            title : String,
-            content : String,
-            picture : String,
-            video : String,
-            author: String,
-            date: String
+        models.News = db.define("news", {
+            id: Number,
+            title: String,
+            content: String,
+            pictureUrl: String,
+            videoUrl: String,
+            date: String,
+            author: String
         });
-        models.Blogs = db.define('blogs', {
-            id : Number,
-            title : String,
-            content : String,
-            picture : String,
-            video : String,
-            author: String,
-            date: String
-
+        models.Blogs = db.define("blogs", {
+            id: Number,
+            title: String,
+            content: String,
+            pictureUrl: String,
+            videoUrl: String,
+            date: String,
+            author: String
         });
         next();
     }
 }));
 
+app.use(express.static('../public')); //设置静态文件目录，保证css和js的加载
 
-app.get("/news",function (req, res) {
+app.get('/', function (req, res) {
+    res.sendFile('/home/zl/WebstormProjects/CodingGirlsClub-News/public/login.html')
+});
+
+app.get('/addArticle', function (req, res) {
+    res.sendFile('/home/zl/WebstormProjects/CodingGirlsClub-News/public/addArticle.html');
+});
+
+app.get("/news", function (req, res) {
     let cnt = req.query.count;
     req.models.News.find(function (err, newsInfo) {
-        if(err) throw err;
-        newsInfo.sort(function(input_a,input_b) {
+        if (err) throw err;
+        newsInfo.sort(function (input_a, input_b) {
             let a = input_a.date;
             let b = input_b.date;
             let arr_a = a.split('/');
@@ -70,40 +86,26 @@ app.get("/news",function (req, res) {
                 return arr_b[2] - arr_a[2];
             }
         });
-        if(cnt*6>newsInfo.length){
-            if(cnt*6-6===newsInfo.length){
+        if (cnt * 6 > newsInfo.length) {
+            if (cnt * 6 - 6 === newsInfo.length) {
                 res.status(400).send("没有数据可以返回了");
-            }else{
-                res.send(newsInfo.slice((cnt-1)*6,newsInfo.length));
+            } else {
+                res.send(newsInfo.slice((cnt - 1) * 6, newsInfo.length));
             }
-        }else{
-            res.send(newsInfo.slice((cnt-1)*6,(cnt-1)*6+6));
+        } else {
+            res.send(newsInfo.slice((cnt - 1) * 6, (cnt - 1) * 6 + 6));
         }
     });
 });
 
-
-app.get('/',urlencodedParser,function (req,res){
-
-});
-app.post('/manage',urlencodedParser,function (req,res) {
-    let login = require('./login');
-    login.findLogin(req,res);
-});
-app.get('/manage/news',urlencodedParser,function (req,res) {
-    let news = require('./getAllNews');
-    news.getAllNews(req,res);
-});
-app.get('/manage/blogs',urlencodedParser,function (req,res) {
-    let blogs = require('./getAllBlogs');
-    blogs.getAllBlogs(req,res);
-});
+let manageRouter = require('./manageRouter');
+app.use('/manage', manageRouter);
 
 var articleRouter = require('./articleRouter');
 app.use('/article', articleRouter);
 
-let email=require('./email');
-app.use('/email',email);
+var uploadRouter = require('./uploadRouter');
+app.use('/upload', uploadRouter);
 
 var server = app.listen(8081, function () {
     var host = server.address().address;
