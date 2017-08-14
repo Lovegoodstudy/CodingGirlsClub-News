0fbd329809fc8a5496afd61acdccf7e50d249c95
JocobZling <924798429@qq.com>
2017-08-14 11:26:28 +0800

    addPicture

--- public/js/addArticle.js
+++ public/js/addArticle.js
@@ -1,36 +1,44 @@
 const BASE_URL = "http://localhost:8081";
-
+/*$(document).ready(function () {
+   $('#addButton').bind('click',function () {
+       $.ajax({
+           url:BASE_URL+
+       })
+   })
+});*/
 $(document).ready(function () {
     $('#submitNews').bind('click', function () {
         $.ajax({
-            url : BASE_URL + '/article?type=news',
-            type : 'post',
-            dataType : 'json',
-            data : {
-                "title" : $('#titleText').val(),
-                "content" : contentEditor.document.getBody().getHtml()
+            url: BASE_URL + '/article?type=news',
+            type: 'post',
+            dataType: 'json',
+            data:{
+                "pictureUrl":new FormData($('#addPictureUrl')[0]),
+                "title": $('#titleText').val(),
+                "content": contentEditor.document.getBody().getHtml()
             },
-            success : function () {
+            success: function () {
                 alert('文章添加成功！');
             },
-            error : function () {
+            error: function () {
                 alert('文章添加失败！');
             }
         });
-    })
+    });
     $('#submitBlogs').bind('click', function () {
         $.ajax({
-            url : BASE_URL + '/article?type=blogs',
-            type : 'post',
-            dataType : 'json',
-            data : {
-                "title" : $('#titleText').val(),
-                "content" : contentEditor.document.getBody().getHtml()
+            url: BASE_URL + '/article?type=blogs',
+            type: 'post',
+            dataType: 'json',
+            data: {
+                "pictureUrl": $('#addPictureUrl').val(),
+                "title": $('#titleText').val(),
+                "content": contentEditor.document.getBody().getHtml()
             },
-            success : function () {
+            success: function () {
                 alert('文章添加成功！');
             },
-            error : function () {
+            error: function () {
                 alert('文章添加失败！');
             }
         });
