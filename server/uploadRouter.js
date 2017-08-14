41e75c6da84c089b7fda4e35f9898de333a046c2
lovegood <287310886@qq.com>
2017-08-14 11:31:38 +0800

    init

--- server/uploadRouter.js
+++ server/uploadRouter.js
@@ -4,13 +4,13 @@ let formidable = require('formidable');
 let fs         = require('fs');
 let path       = require('path');
 
-router.post('/', function (req,res,next) {
+router.post('/', function (req, res, next) {
     let moment = require('moment');
     let folder = moment().format('YYYYMMDD');
     let dirname = __dirname.replace("/server", '/public/upload/' + folder);
-    console.log(dirname);
+    console.log(req);
     if (mkdirsSync(dirname, '0777')) {
-        var form = new formidable.IncomingForm();
+        let form = new formidable.IncomingForm();
         form.encoding = "utf-8";
         form.uploadDir = dirname;
         form.maxFontSize = 2 * 1024 * 1024;
@@ -32,11 +32,38 @@ router.post('/', function (req,res,next) {
         console.error('error')
 });
 
+router.post('/coverImage', function (req, res, next) {
+    let moment = require('moment');
+    let folder = moment().format('YYYYMMDD');
+    let dirname = __dirname.replace("/server", '/public/upload/' + folder);
+    console.log(req);
+    if (mkdirsSync(dirname, '0777')) {
+        let form = new formidable.IncomingForm();
+        form.encoding = "utf-8";
+        form.uploadDir = dirname;
+        form.maxFontSize = 2 * 1024 * 1024;
+        form.parse(req, function (err, fields, files) {
+            if (err)return;
+            //console.log(files);
+            let fileName = moment().format() + '-' + files.upload.name;
+            //console.log(fileName);
+            var newPath = form.uploadDir + "/" + fileName;
+            //console.log(newPath);
+            fs.rename(files.upload.path, newPath, function () {
+                let url = '/upload/' + folder + "/" + fileName;
+                //res.write(`<script type="text/javascript">window.parent.CKEDITOR.tools.callFunction(${req.query.CKEditorFuncNum}, \'${url}\');</script>`);
+                //JSON.stringify({fileName: '/upload/' + folder + "/" + fileName})
+                res.end({fileName: '/upload/' + folder + "/" + fileName});
+            });
+        })
+    }
+})
+
 function mkdirsSync(dirpath, mode) {
     if (!fs.existsSync(dirpath)) {
         var pathtmp;
         dirpath.split(path.sep).forEach(function (dirname) {
-            if (dirname == "") {
+            if (dirname === "") {
                 dirname = "/"
             }
             if (pathtmp) {
